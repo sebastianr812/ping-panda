f30d7dff4c3e64c7cea84d450de998554b92fc63
Sebastian Rojas <sebastian.rojas.dev@gmail.com>
2024-12-07 16:17:24 -0500

    feat: started landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,42 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query"
-import { RecentPost } from "./components/post"
-import { client } from "./lib/client"
+import { Heading } from "./components/heading";
+import { MaxWidthWrapper } from "./components/max-width-wrapper";
 
-export default async function Home() {
-  const res = await client.post.recent.$get()
-  const recentPost = await res.json()
+export default function Page() {
+    return (
+        <>
+            <section className="relative py-24 sm:py-32 bg-brand-25">
+                <MaxWidthWrapper className="text-center">
+                    <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+                        <div>
+                            <Heading>
+                                <span>
+                                    Real-Time SaaS Insights,
+                                </span>
+                                <br />
+                                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                                    Delivered to Your Discord
+                                </span>
+                            </Heading>
+                        </div>
+                        <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+                            PingPanda is the easiest way to monitor your SaaS.
+                            Get instant notifications for <span
+                                className="font-semibold text-gray-700">sales, new
+                                users, or any other event {' '}</span>sent directly to
+                            your Discord.
+                        </p>
+                        <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-center sm:items-start">
 
-  /**
-   * This is the intended way to prefetch data on the server to have it immediately available in the client.
-   * But: you could also just pass the post as a prop instead of using the `HydrationBoundary`
-   */
-  const queryClient = new QueryClient()
-
-  await queryClient.prefetchQuery({
-    queryFn: () => recentPost,
-    queryKey: ["get-recent-post"],
-  })
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-medium font-heading tracking-tight sm:text-[5rem]">
-            <span className="text-brand-700">j</span>stack
-          </h1>
-
-          <p className="text-center max-w-prose text-balance">
-            The modern way to confidently ship high-performance, low-cost
-            Next.js apps. End-to-end typesafe with an incredible DX.
-          </p>
-
-          <RecentPost />
-        </div>
-      </main>
-    </HydrationBoundary>
-  )
+                        </ul>
+                    </div>
+                </MaxWidthWrapper>
+            </section>
+            <section>
+            </section>
+            <section>
+            </section>
+            <section>
+            </section>
+        </>
+    );
 }
