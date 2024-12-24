7b387e5d51edb3e106166fe94d947968ff0a9920
Sebastian Rojas <sebastian.rojas.dev@gmail.com>
2024-12-24 14:23:58 -0500

    work: customer reviews

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,30 @@
 import { Heading } from "@/components/heading";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
-import { Check } from "lucide-react";
+import { Check, Star } from "lucide-react";
 import { ShinyButton } from "@/components/shiny-button";
 import { MockDiscordUI } from "@/components/mock-discord-ui";
 import { AnimatedList } from "@/components/ui/animated-list";
 import { DiscordMessage } from "@/components/discord-message";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function Page() {
+
+    const codeSnippet = `await fetch("http://localhost:3000/api/v1/events", {
+  method: "POST",
+  body: JSON.stringify({
+    category: "sale",
+    fields: {
+      plan: "PRO",
+      email: "zoe.martinez2001@email.com",
+      amount: 49.00
+    }
+  }),
+  headers: {
+    Authorization: "Bearer <YOUR_API_KEY>"
+  }
+})`
     return (
         <>
             {/* HERO */}
@@ -122,6 +139,7 @@ export default function Page() {
                         <Heading>Stay ahead with real-time insights</Heading>
                     </div>
                     <div className="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+
                         {/* First bento element */}
                         <div className="relative lg:row-span-2">
                             <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
@@ -150,10 +168,11 @@ export default function Page() {
                             </div>
                             <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
                         </div>
+
                         {/* second  bento element */}
                         <div className="relative max-lg:row-start-1">
-                            <div className="absolute inset-px rounded-lg bg-white max-lg-rounded-t-[2rem]" />
-                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t[calc(2rem+1px)]">
+                            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                                     <p className="mt-2 text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
                                         Track Any Event
@@ -174,11 +193,117 @@ export default function Page() {
                                     />
                                 </div>
                             </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
+                        </div>
+
+                        {/* third  bento element */}
+                        <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+                            <div className="absolute inset-px rounded-lg bg-white" />
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+                                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                                    <p className="mt-2 text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
+                                        Track Any Properties
+                                    </p>
+                                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                                        Add any custom data you like to an event,
+                                        such as a user email, a purchase amount
+                                        or an exceeded quota.
+                                    </p>
+                                </div>
+                                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                                    <Image
+                                        className="w-full max-lg:max-w-xs"
+                                        src="/bento-custom-data.png"
+                                        alt="Bento box illustrating custom data tracking"
+                                        width={500}
+                                        height={300}
+                                    />
+                                </div>
+                            </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+                        </div>
+
+                        {/* fourth  bento element */}
+                        <div className="relative lg:row-span-2">
+                            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                                    <p className="mt-2 text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
+                                        Easy Integration
+                                    </p>
+                                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                                        Connect PingPanda with your existing
+                                        workflows in minutes and call our
+                                        intuitive logging API from any language.
+                                    </p>
+                                </div>
+                                <div className="relative min-h-[30rem] w-full grow">
+                                    <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                                        <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                                            <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                                                <div className="border-b border-r border-b-white/20 border-r-white/10 px-4 py-2 text-white">
+                                                    pingpanda.js
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="overflow-hidden">
+                                            <div className="max-h-[30rem]">
+                                                <SyntaxHighlighter
+                                                    language="typescript"
+                                                    style={{
+                                                        ...oneDark,
+                                                        'pre[class*="language-"]': {
+                                                            ...oneDark['pre[class*="language-"]'],
+                                                            background: "transparent",
+                                                            overflow: "hidden"
+                                                        },
+                                                        'code[class*="language-"]': {
+                                                            ...oneDark['pre[class*="language-"]'],
+                                                            background: "transparent",
+                                                        }
+                                                    }}>
+                                                    {codeSnippet}
+                                                </SyntaxHighlighter>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
                         </div>
                     </div>
                 </MaxWidthWrapper>
             </section>
-            <section>
+
+            {/* Cusomter reviews */}
+            <section className="relative py-24 sm:py-32 bg-white">
+                <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-20">
+                    <div>
+                        <h2 className="text-center text-base/7 font-semibold text-brand-600">
+                            Real-World Experiences
+                        </h2>
+                        <Heading className="text-center">
+                            What our customers say
+                        </Heading>
+                    </div>
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 divide-y lg:divide-y-0 lg:divide-x divide-gray-200">
+                        {/* first customer review */}
+                        <div className="flex flex-auto flex-col gap-4 bg-brand-25 p-6 sm:p-8 lg:p-16 rounded-t-[2rem] lg:rounded-tr-none lg:rounded-l-[2rem]">
+                            <div className="flex gap-0.5 mb-2 justify-center lg:justify-start">
+                                <Star className="size-5 text-brand-600 fill-brand-600" />
+                                <Star className="size-5 text-brand-600 fill-brand-600" />
+                                <Star className="size-5 text-brand-600 fill-brand-600" />
+                                <Star className="size-5 text-brand-600 fill-brand-600" />
+                                <Star className="size-5 text-brand-600 fill-brand-600" />
+                            </div>
+                            <p className="text-base sm:text-lg lg:text-lg/8 font-medium tracking-tight text-brand-950 text-center lg:text-left text-pretty">
+                                PingPanda has been a game-changer for me. I've
+                                been using it for two months now and seeing sales
+                                pop up in real-time is super satisfying.
+                            </p>
+                        </div>
+                    </div>
+                </MaxWidthWrapper>
             </section>
         </>
     );
