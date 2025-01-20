b76abc389c10aed478bac4ba05144104f3f92faf
Sebastian Rojas <sebastian.rojas.dev@gmail.com>
2025-01-20 14:20:27 -0500

    fix: favicon and README

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-    title: "jStack App",
-    description: "Created using jStack",
+    title: "PingPanda",
+    description: "Real-time alerts, straight to your discord",
     icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
